c19a9c06b5fb4835839417680317115371404602
diegoalvescf <dalvesdocs@gmail.com>
2023-04-15 01:24:26 -0300

    feat: insert eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,30 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+
+import { useState } from 'react';
+
+import viteLogo from '/vite.svg';
+
+import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleCount = () => {};
-
   return (
-    <div className='App'>
+    <div className="App">
       <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-        >
-          <img
-            src={viteLogo}
-            className='logo'
-            alt='Vite logo'
-          />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a
-          href='https://reactjs.org'
-          target='_blank'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
 
-      <div className='card'>
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>+</button>
 
-        <div className='count'>{count}</div>
+        <div className="count">{count}</div>
 
         <button onClick={() => setCount((count) => count - 1)}>-</button>
       </div>
