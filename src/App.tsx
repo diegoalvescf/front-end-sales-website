8859326927e04656c5c333dccf4e8f0f643da531
diegoalvescf <dalvesdocs@gmail.com>
2023-04-15 00:13:26 -0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {};
+
+  return (
+    <div className='App'>
+      <div>
+        <a
+          href='https://vitejs.dev'
+          target='_blank'
+        >
+          <img
+            src={viteLogo}
+            className='logo'
+            alt='Vite logo'
+          />
+        </a>
+        <a
+          href='https://reactjs.org'
+          target='_blank'
+        >
+          <img
+            src={reactLogo}
+            className='logo react'
+            alt='React logo'
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>+</button>
+
+        <div className='count'>{count}</div>
+
+        <button onClick={() => setCount((count) => count - 1)}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
